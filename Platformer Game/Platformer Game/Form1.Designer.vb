df45b3a004214e941093c2e471b12591b6dc0213
Anthony <amsca629@learner.dvusd.org>
2024-02-29 08:18:52 -0700

    2/29/24

---
+++ Platformer Game/Platformer Game/Form1.Designer.vb
@@ -0,0 +1,277 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.tmrRight = New System.Windows.Forms.Timer(Me.components)
+        Me.TmrLeft = New System.Windows.Forms.Timer(Me.components)
+        Me.TmrUp = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrGameLogic = New System.Windows.Forms.Timer(Me.components)
+        Me.TmrGrav = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PicCoin = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.EndPic = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Picplayer = New System.Windows.Forms.PictureBox()
+        Me.Picair = New System.Windows.Forms.PictureBox()
+        Me.PicGround = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.CoinLabel = New System.Windows.Forms.Label()
+        Me.PicSpikeWall = New System.Windows.Forms.PictureBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        CType(Me.PicCoin, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.EndPic, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Picplayer, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Picair, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PicGround, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PicSpikeWall, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'tmrRight
+        '
+        Me.tmrRight.Enabled = True
+        Me.tmrRight.Interval = 60
+        '
+        'TmrLeft
+        '
+        Me.TmrLeft.Interval = 60
+        '
+        'TmrUp
+        '
+        Me.TmrUp.Interval = 60
+        '
+        'tmrGameLogic
+        '
+        '
+        'TmrGrav
+        '
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 16)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "COINS:"
+        '
+        'PicCoin
+        '
+        Me.PicCoin.Image = Global.Platformer_Game.My.Resources.Resources.PXLCoin
+        Me.PicCoin.Location = New System.Drawing.Point(868, 27)
+        Me.PicCoin.Name = "PicCoin"
+        Me.PicCoin.Size = New System.Drawing.Size(100, 50)
+        Me.PicCoin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PicCoin.TabIndex = 11
+        Me.PicCoin.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.BackColor = System.Drawing.Color.Yellow
+        Me.PictureBox5.Location = New System.Drawing.Point(827, 83)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(173, 33)
+        Me.PictureBox5.TabIndex = 8
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackColor = System.Drawing.Color.Yellow
+        Me.PictureBox4.Location = New System.Drawing.Point(1048, 196)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(173, 33)
+        Me.PictureBox4.TabIndex = 7
+        Me.PictureBox4.TabStop = False
+        '
+        'EndPic
+        '
+        Me.EndPic.Image = Global.Platformer_Game.My.Resources.Resources.PiretChests
+        Me.EndPic.Location = New System.Drawing.Point(1341, 336)
+        Me.EndPic.Name = "EndPic"
+        Me.EndPic.Size = New System.Drawing.Size(100, 62)
+        Me.EndPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.EndPic.TabIndex = 6
+        Me.EndPic.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.Yellow
+        Me.PictureBox3.Location = New System.Drawing.Point(1244, 128)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(75, 325)
+        Me.PictureBox3.TabIndex = 5
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Yellow
+        Me.PictureBox2.Location = New System.Drawing.Point(595, 196)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(173, 33)
+        Me.PictureBox2.TabIndex = 4
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Yellow
+        Me.PictureBox1.Location = New System.Drawing.Point(827, 311)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(178, 33)
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'Picplayer
+        '
+        Me.Picplayer.BackColor = System.Drawing.Color.Red
+        Me.Picplayer.Location = New System.Drawing.Point(15, 336)
+        Me.Picplayer.Name = "Picplayer"
+        Me.Picplayer.Size = New System.Drawing.Size(48, 43)
+        Me.Picplayer.TabIndex = 0
+        Me.Picplayer.TabStop = False
+        '
+        'Picair
+        '
+        Me.Picair.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Picair.Location = New System.Drawing.Point(0, -3)
+        Me.Picair.Name = "Picair"
+        Me.Picair.Size = New System.Drawing.Size(1489, 405)
+        Me.Picair.TabIndex = 1
+        Me.Picair.TabStop = False
+        '
+        'PicGround
+        '
+        Me.PicGround.BackColor = System.Drawing.Color.Green
+        Me.PicGround.Location = New System.Drawing.Point(0, 401)
+        Me.PicGround.Name = "PicGround"
+        Me.PicGround.Size = New System.Drawing.Size(1453, 52)
+        Me.PicGround.TabIndex = 2
+        Me.PicGround.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(69, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 16)
+        Me.Label2.TabIndex = 12
+        '
+        'CoinLabel
+        '
+        Me.CoinLabel.AutoSize = True
+        Me.CoinLabel.Location = New System.Drawing.Point(75, 9)
+        Me.CoinLabel.Name = "CoinLabel"
+        Me.CoinLabel.Size = New System.Drawing.Size(14, 16)
+        Me.CoinLabel.TabIndex = 13
+        Me.CoinLabel.Text = "0"
+        '
+        'PicSpikeWall
+        '
+        Me.PicSpikeWall.BackColor = System.Drawing.Color.Red
+        Me.PicSpikeWall.Location = New System.Drawing.Point(1413, 12)
+        Me.PicSpikeWall.Name = "PicSpikeWall"
+        Me.PicSpikeWall.Size = New System.Drawing.Size(40, 303)
+        Me.PicSpikeWall.TabIndex = 14
+        Me.PicSpikeWall.TabStop = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.Yellow
+        Me.TextBox1.Font = New System.Drawing.Font("MS Reference Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(98, 110)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(179, 108)
+        Me.TextBox1.TabIndex = 15
+        Me.TextBox1.Text = "YOU WINN!!!!!"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.TextBox1.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1453, 450)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.PicSpikeWall)
+        Me.Controls.Add(Me.CoinLabel)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.PicCoin)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.EndPic)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Picplayer)
+        Me.Controls.Add(Me.Picair)
+        Me.Controls.Add(Me.PicGround)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PicCoin, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.EndPic, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Picplayer, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Picair, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PicGround, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PicSpikeWall, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Picplayer As PictureBox
+    Friend WithEvents Picair As PictureBox
+    Friend WithEvents PicGround As PictureBox
+    Friend WithEvents tmrRight As Timer
+    Friend WithEvents TmrLeft As Timer
+    Friend WithEvents TmrUp As Timer
+    Friend WithEvents tmrGameLogic As Timer
+    Friend WithEvents TmrGrav As Timer
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents EndPic As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PicCoin As PictureBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents CoinLabel As Label
+    Friend WithEvents PicSpikeWall As PictureBox
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ Platformer Game/Platformer Game/Form1.vb
@@ -0,0 +1,108 @@
+﻿
+Public Class Form1
+    Dim moveSpeed As Integer = 15
+    Dim isJumping As Boolean
+    Private Sub frm2DPlatformer_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        Select Case e.KeyCode
+            Case Keys.D
+                tmrRight.Start()
+            Case Keys.A
+                TmrLeft.Start()
+            Case Keys.W
+                TmrUp.Start()
+                isJumping = True
+        End Select
+    End Sub
+
+    Private Sub tmrRight_Tick(sender As Object, e As EventArgs) Handles tmrRight.Tick
+        Picplayer.Left += moveSpeed
+    End Sub
+
+    Private Sub frm2Dplatformer_KeyUp(sender As Object, e As KeyEventArgs) Handles Me.KeyUp
+        Select Case e.KeyCode
+            Case Keys.D
+                tmrRight.Stop()
+            Case Keys.A
+                TmrLeft.Stop()
+            Case Keys.W
+                TmrUp.Stop()
+                isJumping = False
+        End Select
+    End Sub
+
+    Private Sub tmrLeft_Tick(sender As Object, e As EventArgs) Handles TmrLeft.Tick
+        Picplayer.Left -= moveSpeed
+    End Sub
+
+    Private Sub tmrUp_Tick(sender As Object, e As EventArgs) Handles TmrUp.Tick
+        Picplayer.Top -= moveSpeed
+    End Sub
+
+    Private Sub frm2Dplatformer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        tmrGameLogic.Start()
+    End Sub
+
+    Private Sub tmrGameLogic_Tick(sender As Object, e As EventArgs) Handles tmrGameLogic.Tick
+        If Picplayer.Bounds.IntersectsWith(Picair.Bounds) Then
+            If isJumping = False Then
+                TmrGrav.Start()
+            End If
+        ElseIf Picplayer.Bounds.IntersectsWith(PicGround.Bounds) Then
+            TmrGrav.Stop()
+        End If
+        If Picplayer.Bounds.IntersectsWith(PictureBox1.Bounds) Then
+            TmrGrav.Stop()
+        End If
+        If Picplayer.Bounds.IntersectsWith(PictureBox2.Bounds) Then
+            TmrGrav.Stop()
+        End If
+        If Picplayer.Bounds.IntersectsWith(PictureBox3.Bounds) Then
+            TmrGrav.Stop()
+        End If
+        If Picplayer.Bounds.IntersectsWith(PictureBox4.Bounds) Then
+            TmrGrav.Stop()
+        End If
+        If Picplayer.Bounds.IntersectsWith(PictureBox5.Bounds) Then
+            TmrGrav.Stop()
+        End If
+        If Picplayer.Bounds.IntersectsWith(TextBox1.Bounds) Then
+            Me.Close()
+        End If
+        If Picplayer.Bounds.IntersectsWith(PicSpikeWall.Bounds) Then
+            Me.Close()
+        End If
+        If Picplayer.Bounds.IntersectsWith(PicCoin.Bounds) Then
+            CoinLabel.Text = (+1)
+            PicCoin.Visible = False
+        End If
+        If Picplayer.Bounds.IntersectsWith(EndPic.Bounds) Then
+            Me.BackColor = Color.Green
+            Picair.BackColor = Color.Green
+            PictureBox1.BackColor = Color.Green
+            PictureBox2.BackColor = Color.Green
+            PictureBox3.BackColor = Color.Green
+            PictureBox4.BackColor = Color.Green
+            PictureBox5.BackColor = Color.Green
+            TextBox1.Visible = True
+
+        End If
+
+        For Each b As Control In Me.Controls
+            If TypeOf b Is PictureBox Then
+                If b.Tag = "bound" Then
+                    If Picplayer.Bounds.IntersectsWith(b.Bounds) Then
+                        TmrGrav.Stop()
+                    End If
+                End If
+            End If
+
+        Next
+
+    End Sub
+
+    Private Sub tmrGravity_Tick(sender As Object, e As EventArgs) Handles TmrGrav.Tick
+        Picplayer.Top += moveSpeed
+    End Sub
+
+  
+End Class
